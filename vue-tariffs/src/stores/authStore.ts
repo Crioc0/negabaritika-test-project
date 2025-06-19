8b8157eba3cdd6db5d4605cfc96a330875192b1b
Denis Perekhodko <pirrekd@mail.ru>
2025-06-19 15:20:24 +0300

    Переделал на composition api

--- vue-tariffs/src/stores/authStore.ts
+++ vue-tariffs/src/stores/authStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import { authApi } from '@/api/authApi'
 import { toastService } from '@/services/toastService'
+import { ref } from 'vue'
 
 interface AuthState {
   token: string | null
@@ -8,58 +9,52 @@ interface AuthState {
   error: null | string
 }
 
-export const useAuthStore = defineStore('auth', {
-  state: (): AuthState => ({
-    token: null,
-    status: 'unauthenticated',
-    error: null,
-  }),
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref<string | null>(null)
+  const status = ref<'unauthenticated' | 'authenticated' | 'loading'>('unauthenticated')
 
-  actions: {
-    async loadToken(): Promise<void> {
-      this.status = 'loading'
-      try {
-        this.token = await authApi.getToken()
-        this.status = this.token ? 'authenticated' : 'unauthenticated'
-      } catch (error) {
-        console.error('Failed to load token:', error)
-        this.token = null
-        this.status = 'unauthenticated'
-      }
-    },
+  async function loadToken(): Promise<void> {
+    status.value = 'loading'
+    try {
+      token.value = await authApi.getToken()
+      status.value = token.value ? 'authenticated' : 'unauthenticated'
+    } catch (error) {
+      console.error('Failed to load token:', error)
+      token.value = null
+      status.value = 'unauthenticated'
+    }
+  }
 
-    async isTokenValid(): Promise<boolean> {
-      await this.loadToken()
-      return this.token === 'mock_token'
-    },
+  async function isTokenValid(): Promise<boolean> {
+    await loadToken()
+    return token.value === 'mock_token'
+  }
 
-    async login(username: string, password: string): Promise<void> {
-      this.status = 'loading'
-      try {
-        const response = await authApi.login(username, password)
-        await authApi.saveToken(response.token)
+  async function login(username: string, password: string): Promise<void> {
+    status.value = 'loading'
+    try {
+      const response = await authApi.login(username, password)
+      await authApi.saveToken(response.token)
 
-        this.token = response.token
-        this.status = 'authenticated'
+      token.value = response.token
+      status.value = 'authenticated'
+    } catch (error) {
+      status.value = 'unauthenticated'
+      const errorMessage = error instanceof Error ? error.message : 'Ошибка авторизации'
+      toastService.error(errorMessage)
+    }
+  }
 
+  async function logout(): Promise<void> {
+    try {
+      await authApi.deleteToken()
+      token.value = null
+      status.value = 'unauthenticated'
 
-      } catch (error) {
-        this.status = 'unauthenticated'
-        this.error = error instanceof Error? error.message : 'Ошибка авторизации'
-        toastService.error(this.error)
-      }
-    },
-
-    async logout(): Promise<void> {
-      try {
-        await authApi.deleteToken()
-        this.token = null
-        this.status = 'unauthenticated'
-
-        toastService.info('Вы вышли из системы.')
-      } catch (error) {
-        console.error('Ошибка при выходе:', error)
-      }
-    },
-  },
+      toastService.info('Вы вышли из системы.')
+    } catch (error) {
+      console.error('Ошибка при выходе:', error)
+    }
+  }
+  return { isTokenValid, login, logout, status, token }
 })
--- vue-tariffs/src/stores/tariffStore.ts
+++ vue-tariffs/src/stores/tariffStore.ts
@@ -1,44 +1,38 @@
 import { defineStore } from 'pinia'
 import { toastService } from '@/services/toastService'
 import { initDB } from '@/services/idb'
-import { toRaw } from 'vue'
+import { ref, toRaw } from 'vue'
 import type { Tariff } from '@/types/tariffs'
 
+export const useTariffStore = defineStore('tariff', () => {
+  const tariffs = ref<Tariff[]>([])
+  async function loadTariffs() {
+    try {
+      const db = await initDB()
+      const storedTariffs = await db.get('tariffs', 'list')
+      tariffs.value = storedTariffs || []
+    } catch (error) {
+      console.error('Ошибка при загрузке из IndexedDB:', error)
+      toastService.error('Ошибка при загрузке тарифов.')
+    }
+  }
 
-
-export const useTariffStore = defineStore('tariff', {
-  state: () => ({
-    tariffs: [] as Tariff[],
-  }),
-
-  actions: {
-    async loadTariffs() {
-      try {
-        const db = await initDB()
-        const storedTariffs = await db.get('tariffs', 'list')
-        console.log('Stored tariffs:', storedTariffs)
-        console.log('Current state of tariffs:', this.tariffs)
-        this.tariffs = storedTariffs || []
-      } catch (error) {
-        console.error('Ошибка при загрузке из IndexedDB:', error)
-        toastService.error('Ошибка при загрузке тарифов.')
-      }
-    },
-
-    async addTariff(tariff: Tariff) {
-      try {
-        tariff.processed = true
-        const db = await initDB()
-        this.tariffs.push(tariff)
-        const rawTariffs = toRaw(this.tariffs)
-        await db.put('tariffs', rawTariffs, 'list')
-        toastService.success('Тариф успешно добавлен.')
-      } catch (error) {
-        tariff.processed = false
-        this.tariffs.push(tariff)
-        console.error('Ошибка при сохранении в IndexedDB:', error)
-        toastService.error('Ошибка при сохранении тарифа.')
-      }
-    },
-  },
+  async function addTariff(tariff: Tariff) {
+    try {
+      tariff.processed = true
+      const db = await initDB()
+      tariffs.value.push(tariff)
+      const rawTariffs = toRaw(tariffs.value)
+      await db.put('tariffs', rawTariffs, 'list')
+      toastService.success('Тариф успешно добавлен.')
+    } catch (error) {
+      tariff.processed = false
+      tariffs.value.push(tariff)
+      console.error('Ошибка при сохранении в IndexedDB:', error)
+      toastService.error('Ошибка при сохранении тарифа.')
+    }
+  }
+  return { loadTariffs, addTariff, tariffs}
 })
+
+
